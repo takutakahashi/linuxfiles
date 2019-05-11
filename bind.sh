b135d5870ae5e5a3cc3df97a27b8d9dffa6c6b8d
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2019-05-12 01:51:59 +0900

    mod

--- bind.sh
+++ bind.sh
@@ -5,7 +5,7 @@ prj_root=$PWD
 mkdir -p config`dirname $1`
 cp $1 config/$1
 
-mkdir -p backups`dirname $1`
-mv $1 backups/$1
+mkdir -p backups/`hostname`/`dirname $1`
+mv $1 backups/$1 || (
 
 ln -s $prj_root/config/$1 $1
