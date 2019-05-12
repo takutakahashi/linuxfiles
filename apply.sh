23af003ed3ab17c58c87b8f77c1ff9e418731e9c
Takuya TAKAHASHI <takahashi@bank.co.jp>
2019-05-12 13:58:16 +0900

    apply

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 prj_root=$PWD
 mkdir -p backups/`hostname`/`dirname $1`
-mv $1 backups/$1 || (sudo ./cp.sh $1; exit 0)
-
+mv $1 backups/`hostname`$1
+mkdir -p `dirname $1`
 ln -s $prj_root/config/`hostname`$1 $1
--- bind.sh
+++ bind.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
 
-prj_root=$PWD
 mkdir -p config/`hostname``dirname $1`
 cp $1 config/`hostname`$1
